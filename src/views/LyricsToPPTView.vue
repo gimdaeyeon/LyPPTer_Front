70f11745b4421197e2915cca49f074f08c6e7174
gimdaeyeon <rlaeodus1306@naver.com>
2025-02-04 23:56:07 +0900

    Design: inline 스타일 정리

--- src/views/LyricsToPPTView.vue
+++ src/views/LyricsToPPTView.vue
@@ -1,40 +1,23 @@
 <template>
   <div class="gap-1 px-6 flex flex-1 justify-center py-5">
     <div class="layout-content-container flex max-w-[1200px] flex-1">
-      <div class="overflow-hidden flex-[20_1_0]">
-        <div class="h-full overflow-y-auto border-r border-gray-200">
-          <div class="space-y-2 p-2">
-            <div class="slide-item-box border-primary">
-              <img class="absolute h-full w-full" alt="Slide 1" loading="lazy" decoding="async"
-                   src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww">
-              <div class="slide-number">1</div>
-            </div>
-            <div class="slide-item-box border-transparent">
-              <img class="absolute h-full w-full" alt="Slide 2" loading="lazy" decoding="async" data-nimg="fill"
-                   src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2h8ZW58MHx8MHx8fDA%3D">
-              <div class="slide-number">2</div>
-            </div>
-            <div class="slide-item-box border-transparent">
-              <img class="absolute h-full w-full" alt="Slide 3" loading="lazy" decoding="async"
-                   src="https://images.unsplash.com/photo-1449824913935-59a10b8d2000?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2l0eSUyMHNreWxpbmV8ZW58MHx8MHx8fDA%3D">
-              <div class="slide-number">3</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SlidePreviewList/>
       <div class="overflow-hidden flex-[80_1_0]">
         <div class="h-full p-4">
-          <div class="mt-2"><h2 class="text-xl font-semibold mb-4">Adjust Layout</h2>
-            <div class="aspect-video relative overflow-hidden rounded-lg border border-gray-200"><img
-                alt="Mountain Landscape" loading="lazy" decoding="async" data-nimg="fill" class="object-cover"
-                src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww"
-                style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;">
-              <div
-                  style="position: absolute; left: 44.0601%; top: 7.76009%; width: 200px; height: 100px; border: 2px dashed rgb(0, 171, 107); cursor: move; overflow: hidden;">
-                <textarea
-                    style="width: 100%; height: 100%; resize: none; border: none; background: transparent; outline: none; color: white; text-shadow: black 1px 1px 2px; font-size: 16px; padding: 5px;">Enter your text here</textarea>
-                <div
-                    style="position: absolute; right: -5px; bottom: -5px; width: 10px; height: 10px; background: rgb(0, 171, 107); cursor: se-resize;"></div>
+          <div>
+            <input class="flex h-10 w-full rounded-md border border-input border-gray-200 bg-background px-3
+            py-2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 disabled:cursor-not-allowed
+            disabled:opacity-50 text-lg mb-4" placeholder="Enter presentation title" type="text">
+            <div class="aspect-video relative overflow-hidden rounded-lg border border-gray-200">
+              <img class="absolute h-full w-full"
+                   alt="Mountain Landscape" loading="lazy" decoding="async" data-nimg="fill"
+                src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww">
+              <div class="absolute w-[200] h-[100] border-2 border-dashed border-primary cursor-move overflow-hidden"
+                  style="left: 44.0601%; top: 7.76009%;">
+                <textarea class="w-full h-full resize-none border-none text-white text-base p-1"
+                    >Enter your text here</textarea>
+                <div class="absolute size-[10px] bg-primary cursor-se-resize"
+                    style="right: -5px; bottom: -5px;"></div>
               </div>
             </div>
           </div>
@@ -48,6 +31,7 @@
 <script setup>
 
 import LyricsArea from "@/components/LyricsArea.vue";
+import SlidePreviewList from "@/components/SlidePreviewList.vue";
 </script>
 
 <style scoped>
