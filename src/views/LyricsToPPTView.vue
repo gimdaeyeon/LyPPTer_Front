79dd3a4bbfea6a9e25e0c4bb4b7c57b3f0edce18
gimdaeyeon <rlaeodus1306@naver.com>
2025-02-12 23:51:50 +0900

    Feat: fabric.js 기본적인 텍스트 박스 구성

--- src/views/LyricsToPPTView.vue
+++ src/views/LyricsToPPTView.vue
@@ -8,20 +8,20 @@
             <input class="flex h-10 w-full rounded-md border border-input border-gray-200 bg-background px-3
             py-2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 disabled:cursor-not-allowed
             disabled:opacity-50 text-lg mb-4" placeholder="Enter presentation title" type="text">
-            <div class="aspect-video relative overflow-hidden rounded-lg border border-gray-200">
+            <div ref="canvasContainer" class="aspect-video relative overflow-hidden rounded-lg border border-gray-200">
               <canvas ref="canvas"></canvas>
-<!--              <img class="absolute h-full w-full"-->
-<!--                   alt="Mountain Landscape" loading="lazy" decoding="async" data-nimg="fill"-->
-<!--                   src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww">-->
-<!--              <div class="absolute w-[200] h-[100] border-2 border-dashed border-primary cursor-move overflow-hidden"-->
-<!--                   style="left: 44.0601%; top: 7.76009%;">-->
-<!--                <textarea class="w-full h-full resize-none border-none text-white text-base p-1"-->
-<!--                >Enter your text here</textarea>-->
-<!--                <div class="absolute size-[10px] bg-primary cursor-se-resize"-->
-<!--                     style="right: -5px; bottom: -5px;">-->
+              <!--              <img class="absolute h-full w-full"-->
+              <!--                   alt="Mountain Landscape" loading="lazy" decoding="async" data-nimg="fill"-->
+              <!--                   src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww">-->
+              <!--              <div class="absolute w-[200] h-[100] border-2 border-dashed border-primary cursor-move overflow-hidden"-->
+              <!--                   style="left: 44.0601%; top: 7.76009%;">-->
+              <!--                <textarea class="w-full h-full resize-none border-none text-white text-base p-1"-->
+              <!--                >Enter your text here</textarea>-->
+              <!--                <div class="absolute size-[10px] bg-primary cursor-se-resize"-->
+              <!--                     style="right: -5px; bottom: -5px;">-->
 
-<!--                </div>-->
-<!--              </div>-->
+              <!--                </div>-->
+              <!--              </div>-->
             </div>
           </div>
         </div>
@@ -35,22 +35,34 @@
 import LyricsArea from "@/components/LyricsArea.vue";
 import SlidePreviewList from "@/components/SlidePreviewList.vue";
 import {onMounted, ref} from "vue";
-import {Canvas, Text} from "fabric";
+import {Canvas, Textbox,Text } from "fabric";
 
+const canvasContainer = ref();
 const canvas = ref();
+let fabricCanvas;
 
-onMounted(()=>{
 
-  canvas.value = new Canvas(canvas.value);
+onMounted(() => {
+  const {clientWidth, clientHeight} = canvasContainer.value;
 
-  const text = new Text("가사 미리보기", {
+  fabricCanvas = new Canvas(canvas.value, {
+    width: clientWidth,
+    height: clientHeight,
+    // selection: true
+  });
+
+  const text = new Textbox("가사 미리보기", {
+    width:200,
     left: 100,
     top: 100,
     fontSize: 30,
-    fill: "black",
     selectable: true, // 사용자가 클릭하여 조정 가능하도록 설정
+    lockScalingX: false, // x축 크기 조절 가능
+    lockScalingY: false, // y축 크기 조절 가능
   });
-  canvas.value.add(text);
+
+  fabricCanvas.add(text);
+  fabricCanvas.setActiveObject(text);
 });
 
 </script>
