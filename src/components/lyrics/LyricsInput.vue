7df4ba1afe39878d4217429cc784cd390d55c28e
gimdaeyeon <rlaeodus1306@naver.com>
2025-02-18 00:16:24 +0900

    Feat: fontawesome 적용

--- src/components/lyrics/LyricsInput.vue
+++ src/components/lyrics/LyricsInput.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3 h-full">
+  <div class="flex max-w-[480px] flex-wrap gap-4 px-4 py-3 h-full">
     <label class="flex flex-col min-w-40 flex-1 h-full">
         <TextArea
             placeholder="Type or paste lyrics here..."
             class="form-input flex w-full min-w-0 flex-1 resize-none overflow-y-scroll rounded-xl focus:outline-0 border
-            border-gray-200 bg-[#FFFFFF] h-full min-h-36 placeholder:text-[#B88746] p-[15px] text-base font-normal leading-normal"
+            border-gray-200 h-full min-h-36 placeholder:text-[#B88746] p-[15px] text-base font-normal leading-normal"
             v-model="lyrics"
         />
     </label>
--- src/components/lyrics/LyricsSettings.vue
+++ src/components/lyrics/LyricsSettings.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
-    <h1>Settings</h1>
+  <div class="flex flex-wrap gap-2 px-4 py-3 h-full">
+    <div class="w-full">
+      <label class="text-sm font-medium">Font Size</label>
+      <input type="number" class="input mt-2"/>
+    </div>
+    <div class="w-full">
+      <label class="text-sm font-medium">Text Alignment</label>
+      <div class="mt-2 flex gap-2">
+        <button class="border border-gray-200 size-10 rounded-md cursor-pointer hover:bg-gray-100">
+          <i class="fa-solid fa-align-left"></i>
+        </button>
+        <button class="border border-gray-200 size-10 rounded-md cursor-pointer hover:bg-gray-100">
+          <i class="fa-solid fa-align-center"></i>
+        </button>
+        <button class="border border-gray-200 size-10 rounded-md cursor-pointer hover:bg-gray-100">
+          <i class="fa-solid fa-align-right"></i>
+        </button>
+        <button class="border border-gray-200 size-10 rounded-md cursor-pointer hover:bg-gray-100">
+          <i class="fa-solid fa-align-justify"></i>
+        </button>
+      </div>
+    </div>
+    <div class="w-full">
+      <label class="text-sm font-medium">Position</label>
+      <input type="number" class="input mt-2"/>
+    </div>
+    <div class="w-full">
+      <label class="text-sm font-medium">Text Box Size</label>
+      <input type="number" class="input mt-2"/>
+    </div>
+    <div class="w-full">
+      <label class="text-sm font-medium">Background</label>
+      <input type="number" class="input mt-2"/>
+    </div>
   </div>
 </template>
 
