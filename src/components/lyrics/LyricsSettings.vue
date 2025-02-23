d716da1720fbf1a996b132a8dd99d399c8c17fc1
gimdaeyeon <rlaeodus1306@naver.com>
2025-02-23 23:47:16 +0900

    Design: 배경 image 선택 ui 추가. 기본적인 형태의 setting 퍼블리싱 완료

--- src/components/lyrics/LyricsSettings.vue
+++ src/components/lyrics/LyricsSettings.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-wrap gap-2 px-4 py-3 h-full">
     <div class="w-full">
       <label class="text-sm font-medium">Font Size</label>
-      <input type="number" class="input px-3 mt-2"/>
+      <input type="number" class="input px-3 mt-2" v-model="fontSize"/>
     </div>
     <div class="w-full">
       <label class="text-sm font-medium">Text Alignment</label>
@@ -35,8 +35,17 @@
         </div>
       </div>
       <div class="flex gap-2 mt-2">
-        <input type="color" class="color-input" v-model="bgColor"/>
-        <input type="text" class="text-input" v-model="bgColor">
+        <template v-if="isBgImg">
+          <input id="bg-img" class="hidden" type="file" accept="image/*">
+          <label for="bg-img" class="input justify-center items-center gap-2 cursor-pointer hover:bg-gray-100 font-semibold">
+            <i class="fa-solid fa-upload"></i>
+            Choose image
+          </label>
+        </template>
+        <template v-else>
+          <input type="color" class="color-input" v-model="bgColor"/>
+          <input type="text" class="text-input" v-model="bgColor">
+        </template>
       </div>
     </div>
   </div>
@@ -48,12 +57,13 @@ import TextAlignSelector from "@/components/ui/TextAlignSelector.vue";
 import {ref} from "vue";
 import LabeledInput from "@/components/ui/LabeledInput.vue";
 
+const fontSize = ref(24);
 const textAlign = ref('left');
 const positionX = ref(30);
 const positionY = ref(50);
 const textBoxWidth = ref(200);
 const textBoxHeight = ref(200);
-const isBgImg = ref(false);
+const isBgImg = ref(true);
 const bgColor= ref('#000000');
 
 
