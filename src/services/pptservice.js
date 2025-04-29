832e973c460aedace0270c25f7d144b9c71ce16c
gimdaeyeon <rlaeodus1306@naver.com>
2025-04-29 23:00:00 +0900

    Test: ppt생성시 입력된 가사로 ppt 생성

--- src/services/pptservice.js
+++ src/services/pptservice.js
@@ -1,20 +1,31 @@
 import PptxGenJS from "pptxgenjs";
+import {useLyrics} from "@/store/useLyrics.js";
+import {storeToRefs} from "pinia";
+import {toRefs} from "vue";
 
-export function createPPtTest() {
+export function createPPt() {
+    const lyricsStore = useLyrics();
+    const {lyricsSlides} = storeToRefs(lyricsStore);
+    // const {
+    //     fontSize, textBoxWidth, textBoxHeight,
+    //     textColor, bgColor, positionX, positionY, textAlign,
+    //     canvasWidth, canvasHeight,isBgImg,
+    // } = toRefs(lyricsStore.settings);
+
     // 1. Create a Presentation
-    let pres = new PptxGenJS;
+    const pres = new PptxGenJS;
 
-//  2. Add a Slide to the presentation
-    let slide = pres.addSlide();
+    lyricsSlides.value.forEach(lyrics=>{
+        const slide = pres.addSlide();
 
-//     3. Add 1+ object (tables, Shapes, etc.) to the Slide
-    slide.addText("Hello world from PptxGenJs...",{
-        x:1.5,
-        y:1.5,
-        color:"363636",
-        fill:{color:'#F1F1F1'},
-        align: pres.AlignH.center,
-    });
+        slide.addText(lyrics,{
+            x:1.5,
+            y:1.5,
+            color:"363636",
+            fill:{color:'#F1F1F1'},
+            align: pres.AlignH.center,
+        });
+    })
 
 //     4. Save the Presentation
     pres.writeFile({fileName:'Smple.pptx'});
