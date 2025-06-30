12710390db5e3bd1820f26faebdc5f229d9252bc
gimdaeyeon <rlaeodus1306@naver.com>
2025-06-30 23:30:48 +0900

    Design: 로고 및 파비콘 삭제

--- src/layouts/HeaderMenu.vue
+++ src/layouts/HeaderMenu.vue
@@ -1,25 +1,28 @@
 <template>
   <header class="flex items-center justify-between whitespace-nowrap border-b border-b-gray-200 px-10 py-3">
     <router-link to="/" class="flex items-center gap-4 cursor-pointer">
-      <div class="size-4">
-        <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M44 4H30.6666V17.3334H17.3334V30.6666H4V44H44V4Z" fill="currentColor"></path></svg>
-      </div>
-      <h2 class="text-lg font-bold leading-tight tracking-[-0.015em]">Lyppter</h2>
+<!--      logo-->
+<!--      <div class="size-4">-->
+<!--        <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">-->
+<!--          <path d="M44 4H30.6666V17.3334H17.3334V30.6666H4V44H44V4Z" fill="currentColor"></path>-->
+<!--        </svg>-->
+<!--      </div>-->
+      <h2 class="text-lg font-[1000] leading-tight tracking-[-0.015em]">Lyppter</h2>
     </router-link>
     <div class="flex flex-1 justify-end gap-8">
       <div class="flex items-center gap-9">
-<!--        <router-link v-for="menu in menus" class="h-menu-item" :to="menu.link">{{menu.label}}</router-link>-->
+        <!--        <router-link v-for="menu in menus" class="h-menu-item" :to="menu.link">{{menu.label}}</router-link>-->
       </div>
       <div class="flex gap-2">
         <button @click="createPPt"
-            class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-md h-10 px-4 bg-primary hover:bg-[#009960] text-white text-sm font-bold leading-normal tracking-[0.015em]"
+                class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-md h-10 px-4 bg-primary hover:bg-[#009960] text-white text-sm font-bold leading-normal tracking-[0.015em]"
         >
           <span class="truncate">Export</span>
         </button>
       </div>
-<!--      <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10"-->
-<!--          style='background-image: url("https://cdn.usegalileo.ai/sdxl10/7a9a8be9-1695-433a-83f4-faaba9c28106.png");'-->
-<!--      ></div>-->
+      <!--      <div class="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10"-->
+      <!--          style='background-image: url("https://cdn.usegalileo.ai/sdxl10/7a9a8be9-1695-433a-83f4-faaba9c28106.png");'-->
+      <!--      ></div>-->
     </div>
   </header>
 </template>
@@ -30,10 +33,10 @@ import {ref} from "vue";
 import {createPPt} from "@/services/pptService.js";
 
 const menus = ref([
-  {label: 'Home',link:'#'},
-  {label: 'Tutorials',link:'#'},
-  {label: 'FAQs',link:'#'},
-  {label: 'Feedback',link:'#'},
+  {label: 'Home', link: '#'},
+  {label: 'Tutorials', link: '#'},
+  {label: 'FAQs', link: '#'},
+  {label: 'Feedback', link: '#'},
 ]);
 
 </script>
