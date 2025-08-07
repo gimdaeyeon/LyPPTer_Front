15a9aff6f5a84c3132c140d3304df95eb0aabb98
gimdaeyeon <rlaeodus1306@naver.com>
2025-08-07 20:55:40 +0900

    Fix: 가사 중간에서 삭제시 커서가 맨 끝으로 이동되는 현상 수정

--- src/components/lyrics/LyricsInput.vue
+++ src/components/lyrics/LyricsInput.vue
@@ -5,7 +5,7 @@
             placeholder="Type or paste lyrics here..."
             class="text-area form-input flex w-full min-w-0 flex-1 resize-none overflow-y-scroll rounded-xl focus:outline-0 border
             border-gray-200 h-full min-h-36 placeholder:text-secondary p-[15px] text-base font-normal leading-normal"
-            v-model="lyrics"
+            v-model="localLyrics"
         />
     </label>
   </div>
@@ -17,21 +17,30 @@ import TextArea from "@/components/common/tag/TextArea.vue";
 
 import {useLyrics} from "@/store/useLyrics.js";
 import {storeToRefs} from "pinia";
-import {useTemplateRef} from "vue";
+import {nextTick, onMounted, ref, useTemplateRef, watch} from "vue";
 
 const lyricsStore = useLyrics();
 const {lyrics, currentSlideIdx} = storeToRefs(lyricsStore);
 const textarea = useTemplateRef('textarea');
+const localLyrics = ref('');
 
 // 가사 입력창에서 입력 커서의 이동에 따라 현재 슬라이드 번호를 변경하는 함수
-function setCurrentSlide(){
-  // if (!textarea.value) return
+async function setCurrentSlide(){
+  if (!textarea.value) return
   const pos = textarea.value.textarea.selectionStart
   /* 커서 앞 문자열 */
-  const before = lyrics.value.slice(0, pos)
+  const before = localLyrics.value.slice(0, pos)
   /* 구분자(엔터 2줄↑)로 split → 배열 길이 - 1 == 슬라이드 인덱스 */
-  currentSlideIdx.value = before.split(/(?:\r?\n){2,}/).length -1
+  currentSlideIdx.value = before.split(/(?:\r?\n){2,}/).length -1;
 }
+
+onMounted(()=>{
+  localLyrics.value = lyrics.value;
+});
+
+watch(localLyrics,(newLyrics)=>{
+  lyrics.value = newLyrics;
+});
 
 </script>
 
